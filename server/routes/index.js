0539b44cfc54f5b0884e9f36eceac1df3dafe0ac
Ahmed Shatat <a.shatat@hotmail.com>
2019-07-20 16:44:33 +0300

    refactor routes
    remove all the controlers function and import it from the controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,62 +1,23 @@
 const express = require('express');
 const { Router } = express; //Here we destructure (ES6) the Router value off of express
 const router = Router();
-const queries = require('./queries');
 
-//These routes are all promise based - promises are native feature of javascript that provide an alternative way of handling
-//asynchronous actions. You make a request and use the '.then()' syntax to handle the response.
+const {
+  getAll,
+  getSingleFacster,
+  addFacster,
+  getFacsterHobby,
+  getFacsterSuperpower
+} = require('../controllers');
 
-// =========================================================
-// Routes
-// =========================================================
+router.get('/facsters', getAll);
 
-router.get('/facsters', (req, res, next) =>
-  queries
-    .getAll()
-    .then(users => res.status(200).json(users))
-    .catch(err => next(err))
-);
-/*
- * '/facsters/:name'
- */
-router.get('/facsters/:name', ({ params: { name } }, res, next) => {
-  queries
-    .getSingleFacster(name)
-    .then(person => res.status(200).json(person))
-    .catch(err => next(err));
-});
+router.get('/facsters/:name', getSingleFacster);
 
-/*
- * '/facster/new'
- */
-router.post('/facster/new', ({ body }, res, next) => {
-  queries
-    .addFacster(body)
-    .then(userID => queries.getFacsterById(userID))
-    .then(user => res.status(201).json(user))
-    .catch(err => next(err));
-});
+router.post('/facster/new', addFacster);
 
-/*
- * '/facsters/:name/hobby'
- */
-router.get('/facsters/:name/hobby', ({ params: { name }, body }, res, next) => {
-  queries
-    .getFacsterHobby(name)
-    .then(facsterAndHobby => res.status(200).json(facsterAndHobby))
-    .catch(err => next(err));
-});
-/*
- * '/facsters/:name/superpower'
- */
-router.get(
-  '/facsters/:name/superpower',
-  ({ params: { name }, body }, res, next) => {
-    queries
-      .getFacsterSuperpower(name)
-      .then(facsterAndPower => res.status(200).json(facsterAndPower))
-      .catch(err => next(err));
-  }
-);
+router.get('/facsters/:name/hobby', getFacsterHobby);
+
+router.get('/facsters/:name/superpower', getFacsterSuperpower);
 
 module.exports = router;
